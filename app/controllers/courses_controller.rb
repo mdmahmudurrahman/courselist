b1c43c13c117802f13d740ea6b9d7637bbcb875e
mdmahmudurrahman <mailtommrr@gmail.com>
2022-08-06 12:37:28 +0200

    Add presenter

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,13 @@
 class CoursesController < ApplicationController
   def index
+    # Base url can be obtained from ENV variable
+    # API wrapper could be used here to make the external API call
     uri = URI('https://open.hpi.de/bridges/moochub/courses')
-    
+
     begin
       response = Net::HTTP.get(uri)
-      @courses = JSON.parse(response)["data"]
+      @response = JSON.parse(response)["data"]
+      @courses = Api::CoursePresenter.new(@response).format_response
       @courses = Kaminari.paginate_array(@courses).page(params[:page])
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => error
